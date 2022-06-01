de2933b45f55609e32d93a6be1559227aa7d44c8
“erikayanez“ <erikajoam@gmail.com>
2022-06-01 13:56:23 +0200

    Applying the new option to the youtube project

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const div = document.querySelector(".main");
+// div.classList.add("big");
+div.classList.add("small", "big");
+console.log(div.classList);
+div.classList.remove("big");
+const classArr = ["classOne", "classTwo"];
+div.classList.add(...classArr);
+const theme = () => {
+  const checkbox = document.querySelector("#light-dark").checked;
+  const body = document.querySelector("body");
+  console.log(checkbox);
+  if (checkbox) {
+    //  this is cool state
+    // body.style.backgroundColor = "black";
+    // body.classList.toggle("dark");
+    body.classList.add("dark");
+    body.classList.remove("light");
+  } else {
+    //  this is not so cool state
+    // body.style.backgroundColor = "white";
+    // body.classList.toggle("light");
+    body.classList.add("light");
+    body.classList.remove("dark");
+  }
+};
